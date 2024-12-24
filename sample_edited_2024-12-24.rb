9b28eb80f22c72490e20d2c991decbd808f03eaa
swat9013 <12694496+swat9013@users.noreply.github.comn>
2024-12-24 18:02:31 +0900

    add

---
+++ sample_edited_2024-12-24.rb
@@ -0,0 +1,106 @@
+class User
+  attr_reader :user_id, :email, :type
+
+  UserType={
+    "1" => "CUSTOMER",
+    "2" => "EMPLOYEE"
+  }
+
+  def initialize(user_id:, email:, type:, is_email_confirmed: false)
+    @user_id = user_id
+    @email = email
+    @name
+    @adress
+    @type = type
+    @is_email_confirmed = is_email_confirmed
+    @domain_events= []
+  end
+
+  def can_change_email()
+      if(@is_email_confirmed)
+        return "Emailアドレス変更できないよ"
+  end
+
+  def change_email(company, new_email)
+    assert (can_change_email == nil)
+
+    if (@email == new_email)
+      return
+
+    new_type = company.is_email_corporate(new_email) ? UserType::EMPLOYEE : UserType::CUSTOMER
+
+    if @type != new_type
+      delta = new_type == UserType::EMPLOYEE ? 1 : -1
+      company.change_number_of_employees(delta)
+    end
+
+    @email = new_email
+    @type = new_type
+    @email_changed_events.add(EmailChangedEvent.new(user_id=@user_id, new_email=new_email))
+  end
+end
+
+class EmailChangedEvent
+  def initialize(user_id:, new_email:)
+    @user_id = user_id
+    @new_email = new_email
+  end
+end
+
+class UserFactory
+  def self.create(user_data)
+    User.new(
+      user_id: user_data[0].to_i,
+      email: user_data[1].to_s,
+      type: User::UserType[user_data[2].to_s]
+      )
+  end
+end
+
+class UserController
+  def change_email(user_id, new_email)
+    user_data = Database.get_user_by_id(user_id)
+    user = UserFactory.create(user_data)
+
+    result = user.can_change_email
+    if (result != nil)
+      return result
+
+    company_data = Database.get_company
+    company = CompanyFactory.create(company_data)
+
+    user.change_email(company, new_email)
+
+    Database.save_company(company)
+    Database.save_user(user)
+    user.email_changed_events.each do |email_changed_event|
+      MessageBus.send_email_changed_message(email_changed_event.user_id, email_changed_event.new_email)
+    end
+  end
+end
+
+class CompanyFactory
+  def self.create(company_data)
+    Company.new(
+      company_domain_name: company_data[0].to_s,
+      number_of_employees: company_data[1].to_i
+    )
+  end
+end
+
+class Company
+  def initialize(company_domain_name:, number_of_employees:)
+    @company_domain_name = company_domain_name
+    @number_of_employees = number_of_employees
+  end
+
+  def is_email_corporate(new_email)
+    email_domain = new_email.split('@')[1]
+    email_domain == company.company_domain_name
+  end
+
+  def change_number_of_employees(delta)
+    assert(@number_of_employees + delta > 0 )
+    @number_of_employees += delta
+  end
+end
